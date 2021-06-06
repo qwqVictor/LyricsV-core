af862d164ddd5ddecb68900696f04f37a35f605d
qwqVictor <i@qwq.ren>
2021-06-06 19:02:02 +0800

    fix(util.escapeFilename): fix the bug which allows unexpected slash

--- src/util/escapeFilename.ts
+++ src/util/escapeFilename.ts
@@ -4,7 +4,9 @@ export function escapeFilename(filename: string) {
     for (let ch of filename) {
         if (invalidChars.indexOf(ch) != -1)
             continue
+        if (ch == '/')
+            ch = '_'
         data.push(ch)
     }
-    return filename.replace('/', '_')
+    return filename
 }
